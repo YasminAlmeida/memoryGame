e08f4c459acdaeb7b1a43d75581929c197aa0d7c
yasmin <yasmin.almeidavd@gmail.com>
2022-11-18 13:53:04 -0400

    feat: Skeleton creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-function App() {
+import * as S from './styles/app.styles'
+import Logo from './assets/img/devmemory_logo.png'
+import {InfoItem} from './components/information'
+const App = () => {
   return (
-    <div className="App">
+    <S.Container>
+        <S.Info>
+          <S.LogoLink href="/">
+            <img src={Logo} width="200" alt="Logo do Jogo"/>
+          </S.LogoLink>
 
-    </div>
+          <S.InfoArea>
+            <InfoItem label="Time" value="00:00"/>
+            <InfoItem label="Moviment" value="0"/>
+          </S.InfoArea>
+          
+          <button>Restart</button>
+        </S.Info>
+        <S.GridArea>
+          ...
+        </S.GridArea>
+    </S.Container>
   );
 }
 
---
+++ src/components/information/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const Container = styled.div``
+export const Label = styled.div`
+  font-size: 1.2rem;
+  color: #6A7D8B;
+`
+export const Value = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  color: #101C40
+`
